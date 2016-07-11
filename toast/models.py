90b99b5dd98ead6344fac027ba38c1cb69f26dcc
mj2p <mj2p.sg@gmail.com>
2016-07-11 21:37:57 +0100

    updated toast

--- toast/models.py
+++ toast/models.py
@@ -1,5 +1,44 @@
 from __future__ import unicode_literals
 
+import requests
+from django.conf import settings
+from django.contrib.auth.models import User
+
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+    )
+    wallet_id = models.IntegerField()
+    mnemonic = models.CharField(
+        max_length=255,
+    )
+    extpub = models.CharField(
+        max_length=255,
+    )
+    level = models.IntegerField(
+        default=0
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(Account, self).__init__(*args, **kwargs)
+
+        self.get_daio_account()
+
+    def get_daio_account(self):
+        r = requests.get(
+            url='{}/new'.format(settings.DAIO_URL),
+            headers={
+                'Authentication': settings.DAIO_TOKEN
+            }
+        )
+        try:
+            response = r.json()
+            self.wallet_id = response['id']
+            self.mnemonic = response['mnemonic']
+            self.extpub = response['extpub']
+        except ValueError:
+            pass
