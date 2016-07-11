891ab7037d083669e8641cb1b937f2637ef9f576
mj2p <mj2p.sg@gmail.com>
2016-07-11 21:37:28 +0100

    updated settings

--- brekky/settings.py
+++ brekky/settings.py
@@ -121,3 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Connection to Daio service for blockchain and transactions
+DAIO_URL = ''
+DAIO_TOKEN = ''
+
+# The pk of the toast user used by butter
+BUTTER_USER_ID = 0
